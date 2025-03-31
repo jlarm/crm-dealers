5f4a2de5b5022ed18f8e46fc6f2af92d72dccfab
jlarm <117611704+jlarm@users.noreply.github.com>
2025-03-31 10:17:22 -0500

    Changed to header layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,3 @@
 <x-layouts.app :title="__('CRM')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div
-            class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 p-5 dark:border-neutral-700"
-        >
-            <livewire:dealership.index />
-        </div>
-    </div>
+    <livewire:dealership.index />
 </x-layouts.app>
